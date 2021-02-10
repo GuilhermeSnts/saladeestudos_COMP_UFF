492784dc00ca6c9fb0ad1cfc50e5f0a21d938378
Guilherme Santos <guilhermess1994@gmail.com>
2021-02-09 21:09:35 -0300

    added lesson list

--- pages/disciplinas/_course/video_aulas.vue
+++ pages/disciplinas/_course/video_aulas.vue
@@ -3,17 +3,20 @@
     <v-flex xs12>
       <video-player></video-player>
     </v-flex>
+    <lesson-list></lesson-list>
   </v-layout>
 </template>
 
 <script>
 import VideoPlayer from '@/components/VideoPlayer'
+import LessonList from '@/components/LessonList'
 
 export default {
   name: 'VideoAulas',
 
   components: {
     'video-player': VideoPlayer,
+    'lesson-list': LessonList,
   },
 }
 </script>
